48eff5f872f9cbd84af71fff821db87b02451269
benjarlett <benjarlett@gmail.com>
2025-07-10 14:45:09 +0100

    Debug: Add pwd to deploy.sh

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -6,9 +6,11 @@ set -e # Exit immediately if a command exits with a non-zero status.
 
 echo "-- Triggering full project refresh --"
 
+echo "[DEBUG] Current directory before cd: $(pwd)"
 # Navigate to the project root and execute refresh.sh
 # The deploy.sh script is executed from the backend directory, so go up one level.
 cd ..
+echo "[DEBUG] Current directory after cd: $(pwd)"
 bash ./refresh.sh
 
 echo "-- Refresh triggered successfully --"
